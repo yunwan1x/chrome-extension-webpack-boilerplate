2b1778a1dbba4117c004f729a8ec700e6c4bd45e
yunwan1x <512458266@qq.com>
2018-09-24 15:53:22 +0800

    Update card.js

--- src/js/popup/bookmark/card.js
+++ src/js/popup/bookmark/card.js
@@ -1,33 +1,19 @@
 import React from "react";
 import {hot} from "react-hot-loader";
 import Tr from "./tr"
-const dateFormat="YYYY-MM-DD HH:mm:ss";
-import {getBread,getHtml,loadSize} from '../util';
 
 class ContentCard extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            loadSize:loadSize,
-        }
     }
 
 
-    componentWillReceiveProps(nextProps){
-        this.setState({loadSize:loadSize});
-    }
-
-
-    componentDidMount() {
-        this.props.onRef(this);
-    }
 
 
     render() {
-        let {urls=[]} = this.props;
-        let {loadSize}=this.state;
+        let {urls=[],loadSize} = this.props;
         return <table  ><tbody>
-        {urls.map((row,rowindex)=>rowindex<loadSize&& <Tr key={row.id} row={row} loadSize={loadSize} {...this.props} rowIndex={rowindex} ></Tr>)}
+        {urls.map((row,rowindex)=>rowindex<loadSize&& <Tr key={row.id} row={row}  {...this.props} rowIndex={rowindex} ></Tr>)}
         </tbody></table>;
     }
 };
