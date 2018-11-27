3798d1b06a38d976a314e2e86056315dd10a0b39
changhui <changhui.wy@gmail.com>
2018-11-27 22:40:12 +0800

    dd

--- src/js/popup/bookmark/tr.js
+++ src/js/popup/bookmark/tr.js
@@ -2,49 +2,49 @@ import React from "react";
 import ReactDom from 'react-dom';
 import {hot} from "react-hot-loader";
 import moment from 'moment';
-import {getBread, getTag, splitTitle,getDomain} from '../util'
-import {bookmark,indexDb,storage,history} from '../../service/chrome';
+import {getBread, getTag, splitTitle, getDomain} from '../util'
+import {bookmark, indexDb, storage, history} from '../../service/chrome';
 import EditableTagGroup from './tag'
-import {Button,Icon,Popconfirm, message,Row,Col,Menu, Dropdown,Input,Divider} from 'antd';
-var self=this;
-const dateFormat="YY/MM/DD HH:mm";
+import {Button, Icon, Popconfirm, message, Row, Col, Menu, Dropdown, Input, Divider} from 'antd';
+
+var self = this;
+const dateFormat = "YY/MM/DD HH:mm";
 
 class Tr extends React.Component {
     constructor(props) {
         super(props);
-        self=this;
-        this.state={edit:false,update:false,title:this.props.row.title};
+        self = this;
+        this.state = {edit: false, update: false, title: this.props.row.title};
     }
 
 
-
-
-    shouldComponentUpdate(nextProps, nextState){
-        if(this.state.update==true)return true;
-        if(nextProps.action&&nextProps.action=='search')return true;
-        if(nextProps.row.id==this.props.row.id)return false;
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.update == true) return true;
+        if (nextProps.action && nextProps.action == 'search') return true;
+        if (nextProps.row.id == this.props.row.id) return false;
         return true;
     }
 
-    viewHistory(url){
-        let {parent}=this.props;
+    viewHistory(url) {
+        let {parent} = this.props;
         parent.setState({
-            current:'history'
-        },()=>{
-            var reg=/https?:\/\/([^\/]*)\/?/;
+            current: 'history'
+        }, () => {
+            var reg = /https?:\/\/([^\/]*)\/?/;
             reg.exec(url);
-            let catchDomain=RegExp.$1;
+            let catchDomain = RegExp.$1;
             parent.refs.bookmark.search(catchDomain);
         })
     }
 
 
-    deleteCallback(v){
-        let tr=v.dom.parentNode;
-        let tbody=tr.parentNode;
+    deleteCallback(v) {
+        let tr = v.dom.parentNode;
+        let tbody = tr.parentNode;
         tbody.removeChild(tr);
     }
-    noTraceView(url){
+
+    noTraceView(url) {
         chrome.windows.create({
             "url": url,
             "incognito": true,
@@ -53,91 +53,116 @@ class Tr extends React.Component {
         });
     }
 
-    async handleClick(node){
-        let children=await bookmark.getChildren(node.id);
-        if(children.length>0){
-            let bread=await getBread(node);
-            this.props.handleClick({node:node,urls:children,bread:bread});
+    async handleClick(node) {
+        let children = await bookmark.getChildren(node.id);
+        if (children.length > 0) {
+            let bread = await getBread(node);
+            this.props.handleClick({node: node, urls: children, bread: bread});
         }
     }
 
-    cancleEdit(row,e){
-        let value=e.target.value;
-        value&&this.setState({edit:false,title:value},()=>value&&bookmark.update(row.id,value))
+    cancleEdit(row, e) {
+        let value = e.target.value;
+        value && this.setState({edit: false, title: value}, () => value && bookmark.update(row.id, value))
     }
 
 
-    render(){
-        let {row,search}=this.props;
-        let {filter,deleteItem,handleClick}=this.props;
-        let {edit,title}=this.state;
-        let _this=this;
-        const menu =(row)=> (
-            <Menu style={{width:200}} >
-                {row.url&&<Menu.Item><a  onClick={filter.bind(this,row,"site")}>site</a></Menu.Item>}
-                {row.url&&<Menu.Item><a  onClick={filter.bind(this,row,"domain")}>domain</a></Menu.Item>}
-                {row.url&&<Menu.Item>
-                    <a size="small" onClick={this.viewHistory.bind(this,row.url)}>view history</a>
-                </Menu.Item>}
-                {row.url&&<Menu.Item>
-                    <a onClick={this.noTraceView.bind(this,row.url)}>无痕浏览</a>
-                </Menu.Item>}
+    render() {
+        let {row, search} = this.props;
+        let {filter, deleteItem, handleClick} = this.props;
+        let {edit, title} = this.state;
+        let _this = this;
+        const menu = (row) => (
+
+            <Menu style={{width: 200}}>
+                <Menu.SubMenu title="bookmark search">
+                    {row.url && <Menu.Item><a onClick={filter.bind(this, row, "site")}>site</a></Menu.Item>}
+                    {row.url && <Menu.Item><a onClick={filter.bind(this, row, "domain")}>domain</a></Menu.Item>}
+                    {row.url && <Menu.Item>
+                        <a size="small" onClick={this.viewHistory.bind(this, row.url)}>view history</a>
+                    </Menu.Item>}
+                    {row.url && <Menu.Item>
+                        <a onClick={this.noTraceView.bind(this, row.url)}>无痕浏览</a>
+                    </Menu.Item>}
+                </Menu.SubMenu>
                 <Divider></Divider>
-                <Menu.Item >
-                        <a size="small" onClick={deleteItem.bind(this,row,this.deleteCallback.bind(this,row))}>delete</a>
-                </Menu.Item>
-                <Menu.Item>
-                    <a size="small" onClick={()=>_this.setState({update:true},()=>this.setState({edit:true},()=>{
-                        let dom=ReactDom.findDOMNode(this.refs.input);
-                        dom.focus();
-                    }))}>edit</a>
-                </Menu.Item>
+                <Menu.SubMenu title="edit bookmark">
+                    <Menu.Item>
+                        <a size="small"
+                           onClick={deleteItem.bind(this, row, this.deleteCallback.bind(this, row))}>delete</a>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <a size="small"
+                           onClick={() => _this.setState({update: true}, () => this.setState({edit: true}, () => {
+                               let dom = ReactDom.findDOMNode(this.refs.input);
+                               dom.focus();
+                           }))}>edit</a>
+                    </Menu.Item>
+                </Menu.SubMenu>
                 <Divider></Divider>
 
                 <Menu.SubMenu title="Google Search">
                     <Menu.Item>
-                        <a target="_blank" href={`https://www.google.com/search?q=site:`+getDomain(row.url)}><span style={{marginRight:"1em"}}><Icon type="google" /></span>site search</a>
+                        <a target="_blank" href={`https://www.google.com/search?q=site:` + getDomain(row.url)}><span
+                            style={{marginRight: "1em"}}><Icon type="google"/></span>site search</a>
                     </Menu.Item>
                     <Menu.Item>
-                        <a target="_blank" href={`https://www.google.com/search?q=intitle:`+row.title}><span style={{marginRight:"1em"}}><Icon type="google" /></span>intitle search</a>
+                        <a target="_blank" href={`https://www.google.com/search?q=intitle:` + row.title}><span
+                            style={{marginRight: "1em"}}><Icon type="google"/></span>intitle search</a>
                     </Menu.Item>
                     <Menu.Item>
-                        <a target="_blank" href={`https://www.google.com/search?q=inurl:`+row.url}><span style={{marginRight:"1em"}}><Icon type="google" /></span>inurl search</a>
+                        <a target="_blank" href={`https://www.google.com/search?q=inurl:` + row.url}><span
+                            style={{marginRight: "1em"}}><Icon type="google"/></span>inurl search</a>
                     </Menu.Item>
                     <Menu.Item>
-                        <a target="_blank" href={`https://www.google.com/search?q=intext:`+row.url}><span style={{marginRight:"1em"}}><Icon type="google" /></span>intext search</a>
+                        <a target="_blank" href={`https://www.google.com/search?q=intext:` + row.url}><span
+                            style={{marginRight: "1em"}}><Icon type="google"/></span>intext search</a>
                     </Menu.Item>
                     <Menu.Item>
-                        <a target="_blank" href={`https://www.google.com/search?q=link:`+row.url}><span style={{marginRight:"1em"}}><Icon type="google" /></span>link search</a>
+                        <a target="_blank" href={`https://www.google.com/search?q=link:` + row.url}><span
+                            style={{marginRight: "1em"}}><Icon type="google"/></span>link search</a>
                     </Menu.Item>
                 </Menu.SubMenu>
             </Menu>
         );
-        let newTitle=this.props.search&&splitTitle(title).title.replace(new RegExp("("+search+")","ig"),"<span style='color: red'>$1</span>");
+        let newTitle = this.props.search && splitTitle(title).title.replace(new RegExp("(" + search + ")", "ig"), "<span style='color: red'>$1</span>");
 
-        const trContent=(row)=>(
+        const trContent = (row) => (
             <tr>
-                <td    ref={(dom)=>{row.dom=dom;}} >
-                    <div draggable="true" ref={dom=>{
-                        if(!dom)return;
-                        dom.oncontextmenu=function(e){
+                <td ref={(dom) => {
+                    row.dom = dom;
+                }}>
+                    <div draggable="true" ref={dom => {
+                        if (!dom) return;
+                        dom.oncontextmenu = function (e) {
                             e.preventDefault();
                         }
                     }}>
-                        {!edit&&<a style={{marginRight:"2em"}}  target="_blank" href={row.url} onClick={this.handleClick.bind(self,row)}>{row.url&&<span style={{width:16,height:16,backgroundImage:`-webkit-image-set(url("chrome://favicon/size/16@1x/${row.url}") 1x, url("chrome://favicon/size/16@2x/${row.url}") 2x)`}}  className="img" ></span>||<Icon  className="img" type="folder" theme="outlined" />}
-                            <span className="wy_title" dangerouslySetInnerHTML={{ __html: newTitle||splitTitle(title).title}}></span>
-                            {!row.url&&<span>&nbsp;({row.children.length})</span>}
-                        </a>||<div style={{marginRight:"2em"}}>
-                            <Input ref="input" style={{lineHeight:2,padding:'0.3em'}} size="small" value={title} onChange={(e)=>this.setState({title:e.target.value})}  onPressEnter={this.cancleEdit.bind(this,row)} onBlur={this.cancleEdit.bind(this,row)}/>
+                        {!edit && <a style={{marginRight: "2em"}} target="_blank" href={row.url}
+                                     onClick={this.handleClick.bind(self, row)}>{row.url && <span style={{
+                            width: 16,
+                            height: 16,
+                            backgroundImage: `-webkit-image-set(url("chrome://favicon/size/16@1x/${row.url}") 1x, url("chrome://favicon/size/16@2x/${row.url}") 2x)`
+                        }} className="img"></span> || <Icon className="img" type="folder" theme="outlined"/>}
+                            <span className="wy_title"
+                                  dangerouslySetInnerHTML={{__html: newTitle || splitTitle(title).title}}></span>
+                            {!row.url && <span>&nbsp;({row.children.length})</span>}
+                        </a> || <div style={{marginRight: "2em"}}>
+                            <Input ref="input" style={{lineHeight: 2, padding: '0.3em'}} size="small" value={title}
+                                   onChange={(e) => this.setState({title: e.target.value})}
+                                   onPressEnter={this.cancleEdit.bind(this, row)}
+                                   onBlur={this.cancleEdit.bind(this, row)}/>
                         </div>}
 
-                        {!edit&&<EditableTagGroup {...this.props} node={row}/>}
+                        {!edit && <EditableTagGroup {...this.props} node={row}/>}
                     </div>
 
                 </td>
-                <td  className="wy_cmd">
+                <td className="wy_cmd">
                     <Dropdown overlay={menu(row)} trigger={['click']}>
-                        <span style={{cursor:'pointer'}} ><span style={{marginRight:"1em"}}>{moment(row.dateAdded).format(dateFormat)}</span> <Icon type="ellipsis" theme="outlined" /></span>
+                        <span style={{cursor: 'pointer'}}><span
+                            style={{marginRight: "1em"}}>{moment(row.dateAdded).format(dateFormat)}</span> <Icon
+                            type="ellipsis" theme="outlined"/></span>
                     </Dropdown>
                 </td>
             </tr>
@@ -146,4 +171,5 @@ class Tr extends React.Component {
         return trContent(row)
     }
 }
+
 export default hot(module)(Tr);
